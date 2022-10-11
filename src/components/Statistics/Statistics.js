a52e3fe028d02835f92e9957d57fa64864007ab8
raf1n <rafinc10@gmail.com>
2022-10-12 03:54:52 +0600

    statistics-component-added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
@@ -7,7 +15,23 @@ const Statistics = () => {
 
   return (
     <div>
-      <h1>Stat</h1>
+      <h1 className="text-center mt-6 mb-6 text-3xl font-semibold">
+        Statistics of Total Quiz Data
+      </h1>
+      <div className="flex justify-center">
+        <LineChart
+          width={800}
+          height={600}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+        </LineChart>
+      </div>
     </div>
   );
 };
